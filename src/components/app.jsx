a9d799cab10cb0744e4a25c7d2b60d340e436918
Mamaev Yuriy <realainov@ya.ru>
2020-06-10 01:32:49 +0300

    feat(Add welcome screen)

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import WelcomeScreen from './welcome-screen.jsx';
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorCount} = props;
+
+  return (
+    <WelcomeScreen
+      errorCount={errorCount}
+    />
+  );
+};
+
+export default App;
