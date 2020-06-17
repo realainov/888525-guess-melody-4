ccde7f2a316aebaa60b08b95366f6303a04f0d08
Mamaev Yuriy <realainov@ya.ru>
2020-06-18 00:33:53 +0300

    feat(Add tests to project)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const onWelcomeButtonClick = () => {};
-
 const App = (props) => {
   const {errorQuantity} = props;
 
   return (
     <WelcomeScreen
       errorQuantity={errorQuantity}
-      buttonClickHandler={onWelcomeButtonClick}
+      buttonClickHandler={() => {}}
     />
   );
 };
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -2,14 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import WelcomeScreen from "./welcome-screen.jsx";
 
-const onWelcomeButtonClick = () => {};
-
 it(`WelcomeScreen should render correctly`, () => {
   const tree = renderer
     .create(
         <WelcomeScreen
           errorQuantity={3}
-          buttonClickHandler={onWelcomeButtonClick}
+          buttonClickHandler={() => {}}
         />
     )
     .toJSON();
